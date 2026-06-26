61b3ef71b853a8d7a1bbada7eacd606276b501e6
Anton Petukhov <petuhova805@gmail.com>
2026-06-26 19:50:47 +0300

    feature: Get RabbitMQ connection settings from env

--- FokySdk/Types/Settings/RabbitMqSettings.cs
+++ FokySdk/Types/Settings/RabbitMqSettings.cs
@@ -8,6 +8,17 @@
         public string Password { get; set; }
         public string Vhost { get; set; }
 
+        public static RabbitMqSettings GetFromEnvironment()
+        {
+            var host = Environment.GetEnvironmentVariable("RABBIT_MQ_HOST") ?? throw new ArgumentException("RABBIT_MQ_HOST env variable is null");
+            var port = Environment.GetEnvironmentVariable("RABBIT_MQ_PORT") ?? throw new ArgumentException("RABBIT_MQ_PORT env variable is null");
+            var user = Environment.GetEnvironmentVariable("RABBIT_MQ_USER") ?? throw new ArgumentException("RABBIT_MQ_USER env variable is null");
+            var password = Environment.GetEnvironmentVariable("RABBIT_MQ_PASSWORD") ?? throw new ArgumentException("RABBIT_MQ_PASSWORD env variable is null");
+            var vhost = Environment.GetEnvironmentVariable("RABBIT_MQ_VHOST") ?? "/";
+
+            return new RabbitMqSettings(host, user, password, ushort.Parse(port ?? "5672"), vhost);
+        }
+
         public RabbitMqSettings(string host, string user, string password, ushort port = 5672, string vhost = "/")
         {
             Host = host;
